1e4aa883181bc8991dc97b99024a1df500dd02c1
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-02-19 18:25:57 -0500

    autorization fix for adding posts and portfolios.

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
     DB_URI: process.env.DB_URI,
-  // DB_URI: 'mongodb://constantine:wertyk88@ds127015.mlab.com:27015/react-portfolio-dev',
     NAMESPACE: 'https://constantine-react-portfolio.herokuapp.com/'
   }
--- server/services/auth.js
+++ server/services/auth.js
@@ -21,7 +21,7 @@ exports.checkRole = role => (
   (req, res, next) => {
     const user = req.user;
 
-    if (user && (user[process.env.NAMESPACE + '/role'] === role)) {
+    if (user && user[NAMESPACE + '/role'] && (user[NAMESPACE + '/role'] === role)) {
       next();
     } else {
       return res.status(401).send({ title: "Not Authorize", description: "You are not authorized" })
