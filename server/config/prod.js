a8e0e14fc13635181a38bbb6bba1aad19b39682f
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-02-19 19:30:35 -0500

    fix namespace for heroku app

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_URI: process.env.DB_URI,
-    NAMESPACE: 'https://constantine-react-portfolio.herokuapp.com/'
+    NAMESPACE: 'https://constantine-react-portfolio.herokuapp.com'
   }
